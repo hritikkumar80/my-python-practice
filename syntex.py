db0064e9e8102325764ba416e493b2b8599c2452
Hritik Kumar <145244200+hritikkumar80@users.noreply.github.com>
2026-01-28 16:11:26 +0530

    Fix output comments and print formatting in syntex.py
    Correct output comments and adjust print statements.

--- syntex.py
+++ syntex.py
@@ -5,3 +5,10 @@ a = 4
 b = 7
 sum = a + b 
 print (sum)
+#output = 11
+
+if 5>8:
+    print ("five is greater than eight")
+else:
+    print ("five is not greater than eight")
+#output = five is not greater than eight
